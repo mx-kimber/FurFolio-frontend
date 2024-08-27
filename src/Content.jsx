c76d203ca02b2d56522b5415442370fb5de4c1c6
Kimber Ogden <mx.kimber@gmail.com>
2024-08-27 01:48:48 -0500

    create Signup component with form, import into Content

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import "./CSS/Global.css"
+import { Signup } from './Signup'
 
 export function Content() {
   return (
-    <div>Content</div>
+    <div>
+      <div>Content</div>
+      
+      <div><Signup /></div>
+    </div>
   )
 }
 
